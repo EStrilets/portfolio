6433a6509b9447bd0b5eae9ba404317f3fb38857
Eugene Strilets <eugenearcher3@gmail.com>
2023-08-08 18:52:48 -0700

    about text

--- app/(about)/AboutSection.tsx
+++ app/(about)/AboutSection.tsx
@@ -21,30 +21,35 @@ const AboutSection = () => {
           <div className="flex flex-col items-start justify-start space-y-4">
             <p className="text-xl font-sans leading-9">
               I've been building products and digital experiences for the web
-              since 2020. My inexhaustible curiosity and love for web technologies and design
-              led me to attend <br/> Simon Fraser University (SFU), where I earned <br/> my
-              Bachelor's Degree of Science with a major <br/> in Computer Science.
+              since 2020. My inexhaustible curiosity and love for web
+              technologies and design led me to attend <br /> <strong className="text-text">Simon Fraser
+              University (SFU)</strong>, where I earned <br /> my Bachelor's Degree of
+              Science with a major <br /> in Computer Science.
             </p>
             <p className="text-xl font-sans leading-9">
               As my first big project, I gathered a team of 3 developers and
               started to develop the full-stack application for the university
-              experience. My experiments and projects eventually got me my first job out of
-              university at <strong className="text-text">Tiggy</strong>.
+              experience. My experiments and projects eventually got me my first
+              job out of university at{" "}
+              <strong className="text-text">Tiggy</strong>.
             </p>
           </div>
         </div>
         <div className="flex-1 ml-2 space-y-4 items-start justify-start">
           <div>
             <p className="text-xl font-sans leading-9">
-              I've worked in various areas of application development, including
-              front-end & full-stuck development, Chrome Extension, mobile and
-              app UI/UX.
+              There I have been working on a delivery app and nd received an
+              incredible amount of skills and knowledge starting from setting up
+              a component library to participating in extensive standup and code
+              reviews. After Tiggy, I got into the web development agency <strong className="text-text">SEO
+              Soul</strong>, where I developed a Chrome extension for company needs and
+              delivered features to clients' projects.
             </p>
           </div>
           <p className="text-xl font-sans leading-9">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry standard dummy text ever
-            since the 1500s, when an unknown printer took a galley of
+            I've worked in various areas of application development, including
+            front-end & full-stuck development, Chrome Extension, mobile and app
+            UI/UX.
           </p>
         </div>
       </div>
